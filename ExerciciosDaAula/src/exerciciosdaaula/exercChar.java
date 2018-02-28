44ce320ae449c6cccf498233e3446a70ce22fa44
brmm211 <brmm211@github.com.br>
2018-02-28 18:49:24 -0300

    char aula
    exercicio char aula

---
+++ ExerciciosDaAula/src/exerciciosdaaula/exercChar.java
@@ -0,0 +1,18 @@
+package exerciciosdaaula;
+
+import java.util.Scanner;
+
+public class exercChar {
+
+    public static void main(String[] args) {
+        String palavra;
+        
+        System.out.println("digite uma palavra que tenha mais de 10 letras: ");
+        palavra = new Scanner(System.in).next();
+        
+        int ind1 = palavra.charAt(0), ind2 = palavra.charAt(9);
+        System.out.println("a palavra é: "+palavra+", tamanho: "+palavra.length());
+        System.out.println("a primeira letra é: "+(char)ind1+", a decima é: "+(char)ind2);
+    }
+
+}
