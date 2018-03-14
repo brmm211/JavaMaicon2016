c7e58d542e8c80fecaea9d297e3c973860b17f61
brmm211 <brmm211@github.com>
2018-03-14 12:38:26 -0300

    exer
    exerc

--- ExerciciosLoops/src/exerciciosloops/ExerciciosLoops02.java
+++ ExerciciosLoops/src/exerciciosloops/ExerciciosLoops02.java
@@ -3,7 +3,7 @@ package exerciciosloops;
 public class ExerciciosLoops02 {
     public static void main(String[] args) {
         int total = 0;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i <= 200; i++) {
             if (i%4==0) {
                 total+=i;
             }
---
+++ ExerciciosLoops/src/exerciciosloops/ExerciciosLoops03.java
@@ -0,0 +1,18 @@
+package exerciciosloops;
+
+public class ExerciciosLoops03 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i < 100; i++) {
+            if (i%3==0 && i%7==0) {
+                total=i;
+                System.out.println(total);
+            }
+            
+        }
+        
+        
+        
+        
+    }
+}
