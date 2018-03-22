dd41f2b389016ab3828356eaeea3ea784542abb5
brmm211 <brmm211@github.com>
2018-03-22 14:37:51 -0300

    pira
    piramide de numeros

--- ExerciciosDaAula/src/exerciciosdaaula2/Exerc03.java
+++ ExerciciosDaAula/src/exerciciosdaaula2/Exerc03.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class Exerc03 {
 
     public static void main(String[] args) {
-        int n;
-//        Scanner cal = new Scanner(System.in);
-//        System.out.println("Ingrese un numero");
-//        n = cal.nextInt();
+        System.out.println("Digite o número");
+        int numero = new Scanner(System.in).nextInt();
 
-
-        for (int i = 0; i <= 7; i++) {
-            for (int k = 2; k < i; k++) {
-                
+//        i = 1  para i menor ou igual x incrementa
+        for (int i = 1; i <= numero; i++) {
+//            j = 1 para menor igual x -1 decrementa
+//              usado para dar os  espaços para formar a
+//              pirâmide sempre imprime um a menos que o número escolhido
+            for (int j = 1; j <= numero - i; j++) {
+                System.out.print("  ");
             }
-                for (int j = 1; j <= i; j++) {       //1 2    2<1 
-                System.out.print(j); 
-
-//                for(int k = 7; k >= 1; k--){
-//                      System.out.print(k);            //2 3    
-//                }    
-
+//            k = i para k menor ou igual 1 decrementa 
+            for (int k = i; k >= 1; k--) {
+//                se k maior ou igual a dez ele imprime apenas o k se não ele imprime o espaço a esquerda depois o k
+                System.out.print((k >= 10) ? +k : " " + k);
             }
-            
-            
-            
-            System.out.println("");
+            for (int k = 2; k <= i; k++) {
+//                esse for começa no dois pois o da esquerda se encarrega de colocar imprimir o 1
+//                se k maior ou igual a dez ele imprime apenas o k se não ele imprime o espaço a esquerda depois o k
+                System.out.print((k >= 11) ? +k : " " + k);
+            }
+            System.out.println();
         }
-       
+
     }
 }
+
