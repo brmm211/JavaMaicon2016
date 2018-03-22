a748c54846b469369e98036746e4bfd0145b6581
brmm211 <brmm211@github.com>
2018-03-21 22:28:05 -0300

    pira
    piramide

---
+++ ExerciciosDaAula/src/exerciciosdaaula2/Exerc03.java
@@ -0,0 +1,33 @@
+package exerciciosdaaula2;
+
+import java.util.Scanner;
+
+public class Exerc03 {
+
+    public static void main(String[] args) {
+        int n;
+//        Scanner cal = new Scanner(System.in);
+//        System.out.println("Ingrese un numero");
+//        n = cal.nextInt();
+
+
+        for (int i = 0; i <= 7; i++) {
+            for (int k = 2; k < i; k++) {
+                
+            }
+                for (int j = 1; j <= i; j++) {       //1 2    2<1 
+                System.out.print(j); 
+
+//                for(int k = 7; k >= 1; k--){
+//                      System.out.print(k);            //2 3    
+//                }    
+
+            }
+            
+            
+            
+            System.out.println("");
+        }
+       
+    }
+}
