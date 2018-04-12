be0f5b221645cfb4d506c0ea68ffa25538b1f5f9
brmm211 <brmm211@github.com>
2018-04-12 20:35:33 -0300

    java
    java

--- AulaProg2Sem/src/aulaprog2sem/Exerc01.java
+++ AulaProg2Sem/src/aulaprog2sem/Exerc01.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Exerc01 {
 
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
+        Scanner lerNome = new Scanner(System.in);
+        Scanner lerNota = new Scanner(System.in);
+        int alunos = 7, notas = 5;
+//        String[][] alunosNotas = new String[alunos][notas];
+        String[] alunosNomes = new String[alunos];
+        double[] notasAlunos = new double[notas];
         
-        String[] alunos = new String[7];
-        double[] notas = new double[5];
-        
-        for (int i = 0; i < alunos.length; i++) {
+        for (int i = 0; i < alunosNomes.length; i++) {
             System.out.printf("Digite o nome do %sº aluno: ", (i+1));
-            alunos[i] = ler.next();
+            alunosNomes[alunos] = lerNome.nextLine();
             System.out.println(i);
-            for (int j = 0; j < notas.length; j++) {
+            for (int j = 0; j < notasAlunos.length; j++) {
                 System.out.printf("digite a %dº nota do %sº aluno: ", (j+1), (i+1));
-                notas[i] = ler.nextDouble();
+                notasAlunos[notas] = lerNota.nextDouble();
                 System.out.println(j);
             }
             
