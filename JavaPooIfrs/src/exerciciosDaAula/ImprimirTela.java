8c7fd7a481257133ccb30b9a9e1905879bc80719
brmm211 <brmm211@github.com>
2018-04-24 20:45:02 -0300

    hj
    metodos

---
+++ JavaPooIfrs/src/exerciciosDaAula/ImprimirTela.java
@@ -0,0 +1,19 @@
+package exerciciosDaAula;
+
+public class ImprimirTela {
+    public static void main(String[] args) {
+        escreverNome();
+        escreverSobrenome();
+        escreverSobrenome2();
+    }
+    public static void escreverNome(){
+        System.out.print("maicon ");
+    }
+    public static void escreverSobrenome(){
+        System.out.print("semensato ");
+    }
+    public static void escreverSobrenome2(){
+        System.out.print("rosa");
+    }
+    
+}
